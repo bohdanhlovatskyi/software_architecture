7a8cc80c1a767db069dd38f35af48133f3662493
bohdanhlovatskyi <bohdan.hlovatskyi@ucu.edu.ua>
2023-04-03 14:07:32 +0300

    little fix

--- logging/domain/message.py
+++ logging/domain/message.py
@@ -2,7 +2,7 @@ import uuid
 from pydantic import BaseModel, Field
 
 def get_str_uuid():
-    return lambda _: str(uuid.uuid4())
+    return str(uuid.uuid4())
 
 class Message(BaseModel):
     uuid: str = Field(default_factory=get_str_uuid)
